5d2d7918d3a22681e3644715e0945621896cf9aa
Saskia1506 <saskiairawan1506@gmail.com>
2026-05-12 22:32:41 +0700

    Saskia Update

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\OnboardingController;
+use App\Http\Controllers\Admin\RestoranController;
+use App\Http\Controllers\Pemilik\TokoController;
 
 //HALAMAN UTAMA (GUEST)
 Route::get('/', function () {
@@ -39,3 +41,32 @@ Route::get('/owner/dashboard', function () {
 Route::get('/admin/dashboard', function () {
     return "Dashboard Admin";
 })->middleware('auth');
+
+// ADMIN
+Route::get('/admin/dashboard', function () {
+    return view('admin.dashboard');
+})->name('admin.dashboard');
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('/restoran',                                 [RestoranController::class, 'list'])->name('restoran.list');
+    Route::get('/restoran/export-csv',                      [RestoranController::class, 'exportCsv'])->name('restoran.export');
+    Route::post('/restoran',                                [RestoranController::class, 'store'])->name('restoran.store');
+    Route::get('/restoran/{id}',                            [RestoranController::class, 'show'])->name('restoran.show');
+    Route::get('/restoran/{id}/data',                       [RestoranController::class, 'editData'])->name('restoran.data');
+    Route::post('/restoran/{id}/update',                    [RestoranController::class, 'update'])->name('restoran.update');
+    Route::delete('/restoran/{id}',                         [RestoranController::class, 'destroy'])->name('restoran.destroy');
+    Route::post('/restoran/{restoranId}/menu',              [RestoranController::class, 'storeMenu'])->name('menu.store');
+    Route::post('/restoran/{restoranId}/menu/{menuId}',     [RestoranController::class, 'updateMenu'])->name('menu.update');
+    Route::delete('/restoran/{restoranId}/menu/{menuId}',   [RestoranController::class, 'destroyMenu'])->name('menu.destroy');
+});
+ 
+// PEMILIK
+Route::prefix('pemilik')->name('pemilik.')->group(function () {
+    Route::get('/toko',                         [TokoController::class, 'index'])->name('toko.index');
+    Route::get('/toko/profil',                  [TokoController::class, 'getProfil'])->name('toko.profil');
+    Route::post('/toko/profil',                 [TokoController::class, 'updateProfil'])->name('toko.profil.update');
+    Route::post('/toko/toggle-buka',            [TokoController::class, 'toggleBuka'])->name('toko.toggle');
+    Route::post('/toko/menu',                   [TokoController::class, 'storeMenu'])->name('menu.store');
+    Route::post('/toko/menu/{id}',              [TokoController::class, 'updateMenu'])->name('menu.update');
+    Route::delete('/toko/menu/{id}',            [TokoController::class, 'destroyMenu'])->name('menu.destroy');
+    Route::post('/toko/menu/{id}/toggle',       [TokoController::class, 'toggleMenu'])->name('menu.toggle');
+});
